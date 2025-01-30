02be346bf95299092b0570e4f8aa195e4664ebf5
Mahfud Portofolio <75352651+mahfudinkerja@users.noreply.github.com>
2025-01-30 07:55:43 +0700

    [perubahan 1] add comment to google.spec.js for clarity

--- tests/google.spec.js
+++ tests/google.spec.js
@@ -4,6 +4,8 @@ import { test, expect } from "@playwright/test";
 test("has title", async ({ page }) => {
         await page.goto("https://google.com/");
 
+        // Perubahan 1
+
         // Expect a title "to contain" a substring.
         await expect(page).toHaveTitle(/Google/);
         await page.close();
