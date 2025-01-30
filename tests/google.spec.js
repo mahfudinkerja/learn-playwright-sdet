607df3c9ed119a082e7435c128544eda5151ec90
Mahfud Portofolio <75352651+mahfudinkerja@users.noreply.github.com>
2025-01-30 08:09:28 +0700

    [perubahan 3]

--- tests/google.spec.js
+++ tests/google.spec.js
@@ -5,6 +5,7 @@ test("has title", async ({ page }) => {
         await page.goto("https://google.com/");
 
         // Perubahan 2
+        // perubahan 3
 
         // Expect a title "to contain" a substring.
         await expect(page).toHaveTitle(/Google/);
